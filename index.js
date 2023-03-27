33649a2a7b27a297c67b6ade7f8146c0baadd449
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-03-27 18:04:22 +0300

    events node js

---
+++ index.js
@@ -0,0 +1,6 @@
+(function(require, module, exports, __filename, __dirname) {
+const userObj = require('./user')
+
+console.log(userObj.user);
+userObj.sayHello();
+})()
---
+++ user.js
@@ -0,0 +1,15 @@
+const user = {
+  name: 'Peter',
+  age: 27,
+};
+
+const user2 = {
+  name: 'Igor'
+}
+
+module.exports = {
+  user: user,
+  sayHello() {
+    console.log('Hello!')
+  }
+};
