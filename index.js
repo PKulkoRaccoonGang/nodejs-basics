550171aa44cca92ae7a00e5cbe4266b3402b42e7
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-03-27 19:46:35 +0300

    http server node js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-(function(require, module, exports, __filename, __dirname) {
-const userObj = require('./user')
+const http = require('http')
 
-console.log(userObj.user);
-userObj.sayHello();
-})()
+const server = http.createServer((request, response) => {
+  console.log(request.url)
+
+  response.write('<h1>Hello from Node JS</h1>')
+  response.write('<h2>Hello from Node JS</h2>')
+  response.write('<h3>Hello from Node JS</h3>')
+
+  response.end(`
+    <div style="background: red">
+      <h1>Test 1</h1>
+    </div>
+  `)
+})
+
+server.listen(3000, () => {
+  console.log('Server is running...')
+})
