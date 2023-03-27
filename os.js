10c484d0f2ef2c7992d4b01a9e1a3503b0e67944
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-03-27 18:03:13 +0300

    os node js

---
+++ os.js
@@ -0,0 +1,22 @@
+const os = require('os')
+
+// Platform
+console.log(os.platform());
+
+// Architecture
+console.log(os.arch());
+
+// Information
+console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+// All memory
+console.log(os.totalmem());
+
+// Root directory
+console.log(os.homedir());
+
+// System time work
+console.log(os.uptime());
