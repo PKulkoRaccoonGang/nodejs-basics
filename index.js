7ea73c9ed3e4db969e6b9975db14f3e286dc9597
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-03-28 12:35:16 +0300

    rest and console app

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((request, response) => {
-  console.log(request.url)
+  if (request.method === 'GET') {
+    response.writeHead(200, {
+      'Content-Type': 'text/html; charset: utf-8'
+    })
 
-  response.write('<h1>Hello from Node JS</h1>')
-  response.write('<h2>Hello from Node JS</h2>')
-  response.write('<h3>Hello from Node JS</h3>')
+    switch (request.url) {
+      case '/':
+        fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf-8', (error, content) => {
+          if (error) throw error
 
-  response.end(`
-    <div style="background: red">
-      <h1>Test 1</h1>
-    </div>
-  `)
+          response.end(content)
+        })
+        break;
+      case '/about':
+        fs.readFile(path.join(__dirname, 'views', 'about.html'), 'utf-8', (error, content) => {
+          if (error) throw error
+
+          response.end(content)
+        })
+        break;
+      case '/api/users':
+        response.writeHead(200, {
+          'Content-Type': 'text/json'
+        })
+        const users = [
+          { name: 'Peter', age: 27 },
+          { name: 'Olena', age: 23 }
+        ]
+        response.end(JSON.stringify(users))
+        break;
+    }
+  } else if (request.method === 'POST') {
+    const body = []
+
+    response.writeHead(200, {
+      'Content-Type': 'text/html; charset: utf-8'
+    })
+
+    request.on('data', data => {
+      body.push(data)
+    })
+
+    request.on('end', () => {
+      const message = body.toString().split('=')[1]
+
+      response.end(`
+        <h1>Your message: ${message}</h1>
+      `)
+    })
+  }
 })
 
 server.listen(3000, () => {
